63df6c08d0e516dd04b36a26d5a8c695aa9b8ba4
Arka Ghosh <39467183+anotherArka@users.noreply.github.com>
2019-12-18 00:37:22 +0530

    Add files via upload

--- Untyped_Parser.hs
+++ Untyped_Parser.hs
@@ -13,18 +13,19 @@ import Data.Bool
 --parseCSV input = parse csvFile "(unknown)" input
 
 -- 
-
+--------------------------------------------------------------------------------------------------------------------------------
+-- is_there a xs = return true is a is inside xs, false otherwise
 is_there :: Char -> String -> Bool
 is_there a [] = False
 is_there a (x : xs) = (x == a) || (is_there a xs)
+
+--------------------------------------------------------------------------------------------------------------------------------
 
 -- separate_by from dont_take taken 
 -- Divides the string "from" using characters in "dont_take" and adds that to "taken"
 -- sample output
 --  separate_by  "abcd" "bc" ["kl","mn"] = ["mn","kla","d"]
 -- separate_by  "abcdefg" "bcf" [] = ["a","de","g"]
-
-
 separate_by :: String -> String -> [String] -> [String]    
 separate_by [] dont_take taken = reverse taken
 separate_by (x : xs) dont_take [] = 
@@ -45,5 +46,21 @@ separate_by (x : xs) dont_take ((y : ys) : yss) =
         separate_by xs dont_take ([] : ((y : ys) : yss))
     else
         separate_by xs dont_take ( ((y : ys) ++ [x]) : yss)
+     
+------------------------------------------------------------------------------------------------------------------   
+        
+--  take_except from dont_take taken
+-- filters the characters in "from" which are in "dont_take"       
+take_except :: String -> String -> String -> String
+take_except [] dont_take taken = taken
+take_except (x : xs) dont_take taken = 
+    if (is_there x dont_take)
+    then
+        take_except xs dont_take taken
+    else
+        take_except xs dont_take (taken ++ [x])          
+
+
+  
         
                  
