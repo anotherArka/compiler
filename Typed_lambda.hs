db0cbbc376f9169fe28ecb9f1d7ad0d892d3e468
arkaghosh <arkasparko@gmail.com>
2020-06-19 15:11:24 +0530

    added top level equality method which does not evaluate

--- Typed_lambda.hs
+++ Typed_lambda.hs
@@ -22,18 +22,42 @@ data Term =
   Pair Term Term |
   Fst Term |
   Snd Term |
-  Lambda String Term |
-  Ind String (Term, [Term])
+  Lambda String Term | -- have to add definition of inductive function
+  Ind String Term [Term]
 
-substitution :: String -> Term -> Term -> Term
-substitution v term (Var u) = term
-substitution v term Unit = Unit
-substitution v term (Inr inside) = Inr (substitution v term inside)
-substitution v term (Inl inside) = Inl (substitution v term inside)
-substitution v term (Pair left right) =
-  Pair (substitution v term left) (substitution v term right)
-substitution v term (Fst inside) = Fst (substitution v term inside)
-substitution v term (Snd inside) = Snd (substitution v term inside)
-substitution v term (Lambda u inside) =
-  if (v == u) then (Lambda u inside) else (Lambda u (substitution v term inside))
+substitute :: String -> Term -> Term -> Term
+substitute v term (Var u) = term
+substitute v term Unit = Unit
+substitute v term (Inr inside) = Inr (substitute v term inside)
+substitute v term (Inl inside) = Inl (substitute v term inside)
+substitute v term (Pair left right) =
+  Pair (substitute v term left) (substitute v term right)
+substitute v term (Fst inside) = Fst (substitute v term inside)
+substitute v term (Snd inside) = Snd (substitute v term inside)
+substitute v term (Lambda u inside) =
+  if (v == u) then (Lambda u inside) else (Lambda u (substitute v term inside))
+substitute v term (Ind cons values nodes) =
+  Ind cons (substitute v term values) (fmap (substitute v term) nodes)
 
+instance Eq Term where
+  (Var this) == (Var that) = this == that
+  Unit == Unit = True
+  (Inr this) == (Inr that) = this == that
+  (Pair a b) == (Pair c d) = (a == c) && (b == d)
+  (Fst (Pair a b)) == (Fst (Pair c d)) = a == c
+  (Fst this) == (Fst that) = (this == that)
+  (Snd (Pair a b)) == (Snd (Pair c d)) = b == d
+  (Snd this) == (Snd that) = (this == that)
+  (Lambda a this) == (Lambda b that) = (a == b) && (this == that)
+  _ == _ = False
+  
+  
+
+evaluate :: Term -> Term
+evaluate (Var u) = (Var u)
+evaluate Unit = Unit
+evaluate (Inr inside) = Inr (evaluate inside)
+evaluate (Inl inside) = Inl (evaluate inside)
+evaluate (Pair left right) = Pair (evaluate left) (evaluate right)  
+evaluate (Fst (Pair left right)) = left
+evaluate (Snd (Pair left right)) = right
