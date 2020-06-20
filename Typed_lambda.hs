ea7914b7d023d667cc6eba2f0378d74b324d2bf2
arkaghosh <arkasparko@gmail.com>
2020-06-20 23:24:31 +0530

    started writing parser

--- Typed_lambda.hs
+++ Typed_lambda.hs
@@ -1,3 +1,5 @@
+module Typed_lambda where
+
 data Nat = Zero | Succ Nat
 
 data TType =
@@ -20,10 +22,10 @@ instance Eq TType where
 
 instance Show TType where
   show Empty = "Empty"
-  show Singleton = "()"
+  show Singleton = "Unit"
   show (Sum a b) = (show a) ++ " + " ++ (show b)
   show (Product a b) = "(" ++ (show a) ++ "," ++ (show b) ++ ")"
-  show (Function a b) = "(" ++ (show a) ++ "->" ++ (show b) ++ ")" 
+  show (Function a b) = "(" ++ (show a) ++ " -> " ++ (show b) ++ ")" 
 
 data Term = 
   Var String |
@@ -32,7 +34,8 @@ data Term =
   Inl Term TType | -- If x : T then (Inl x S) : Sum T S
   Pair Term Term |
   Lambda String TType Term | 
-  App Term Term  
+  App Term Term |
+  Void TType 
   -- have to add definition of inductive function and application of a function
   -- Ind String Term [Term]
 
@@ -44,17 +47,19 @@ instance Eq Term where
   (Pair a b) == (Pair c d) = (a == c) && (b == d)
   (Lambda a u this) == (Lambda b v that) = (u == v) && (a == b) && (this == that)
   (App f x) == (App g y) = (f == g) && (x == y)
+  (Void s) == (Void t) = (s == t)
   -- (Ind c u s) == (Ind d v t) = (c == d) && (u == v) && (s == t)
   _ == _ = False
 
 instance Show Term where
   show (Var x) = x
-  show Unit = "()"
+  show Unit = "unit"
   show (Inr x t) = "Inr(" ++ (show x) ++ ")"
   show (Inl x t) = "Inl(" ++ (show x) ++ ")"
   show (Pair x y) = "(" ++ (show x) ++ "," ++ (show y) ++ ")"
-  show (Lambda x t inside) = "\\" ++ x ++ " : " ++ (show t) ++ " . " ++ (show inside)
+  show (Lambda x t inside) = "/" ++ x ++ " : " ++ (show t) ++ " . " ++ (show inside)
   show (App x y) = "(" ++ (show x) ++ (show y) ++ ")"
+  show (Void t) = "void"
 
 substitute :: String -> Term -> Term -> Term
 substitute v term (Var u) = term
@@ -65,7 +70,8 @@ substitute v term (Pair left right) =
   Pair (substitute v term left) (substitute v term right)
 substitute v term (Lambda u t inside) =
   if (v == u) then (Lambda u t inside) else (Lambda u t (substitute v term inside))
-substitute v term (App func args) = App (substitute v term func) (substitute v term args)  
+substitute v term (App func args) = App (substitute v term func) (substitute v term args)
+substitute v term (Void t) = Void t  
 -- substitute v term (Ind cons values nodes) =
 --   Ind cons (substitute v term values) (fmap (substitute v term) nodes)
 
@@ -84,7 +90,8 @@ evaluate (App func args) = let
   in
   if ((func == func_1) && (args == args_1)) then (App func args)
     else (evaluate (App func_1 args_1))
-
+evaluate (Void t) = Void t
+ 
 find_type_of_var :: String -> [(String, TType)] -> (Either String TType)
 find_type_of_var x [] = Left ("Not found variable" ++ x)
 find_type_of_var x (v : vs) =
@@ -107,4 +114,5 @@ type_check (App x y) vs =
       (\s -> if (u == s) then Right(v) else
         Left("The function " ++ (show x) ++ " : " ++ (show (Function u v)) 
         ++ " has argument " ++ (show y) ++ " : " ++ (show v))))
-    _ -> Left("Not a function : " ++ (show x)))  
+    _ -> Left("Not a function : " ++ (show x)))
+type_check (Void tt) vs = Right (Function Empty tt)     
---
+++ Typed_parser.hs
@@ -0,0 +1,34 @@
+module Typed_parser where
+
+import Typed_lambda
+import Untyped_Parser
+
+import Data.Char
+--import Text.ParserCombinators.Parsec
+import Data.List
+import Data.Bool
+import Data.Maybe
+
+data Context_Entry = Entry {
+    name :: String ,
+    ttype :: TType ,
+    value :: Term
+  }
+
+-- These characters should not be part of a word
+invalid :: [Char]
+invalid = ",:;!@#$%^&*" 
+
+-- contains list to_check checks if there is a character in to_check
+-- which is also in list 
+contains :: String -> String -> Bool
+contains [] to_check = False
+contains (ch : chs) to_check =
+  if (elem ch to_check) then True else (contains chs to_check)
+
+-- counts number of occurences of an element in a list
+count :: Eq a => a -> [a] -> Int
+count x [] = 0
+count x (y : ys) = if (x == y) then (1 + (count x ys)) else (count x ys)
+
+     
